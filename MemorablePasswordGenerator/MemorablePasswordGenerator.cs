021e297674eb38b3e504c35033b4ac30d38c20a9
Gökhan Witteborn <goekhan.witteborn@gmx.de>
2018-11-26 23:11:26 +0100

    Project Setup, and example lists

---
+++ MemorablePasswordGenerator/MemorablePasswordGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace MemorablePasswordGenerator
+{
+    internal class MemorablePasswordGenerator
+    {
+        private static int getLineCount(string wordlist_Filepath) => File.ReadLines(wordlist_Filepath).Count();
+
+        public static string GenerateWords(string wordlist_Filepath, int wordcount)
+        {
+            Random rnd = new Random();
+            int numLines = getLineCount(wordlist_Filepath);
+            string pw = "";
+            for (int i = 0; i < wordcount; i++)
+            {
+                pw += " " + File.ReadLines(wordlist_Filepath).Skip(rnd.Next(numLines - 1)).Take(1).First();
+            }
+            return pw;
+        }  
+    }
+}
--- MemorablePasswordGenerator/Program.cs
+++ MemorablePasswordGenerator/Program.cs
@@ -2,11 +2,50 @@
 
 namespace MemorablePasswordGenerator
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.Write("What is the name of the wordlist you want to use? (ex. wordlist.txt)" +
+                "\n>");
+            string listname = Console.ReadLine();
+
+            if (!System.IO.File.Exists(listname))
+            {
+                Console.WriteLine("I can not find the given file, please try again. :(\n");
+                Main(null);
+            }
+
+            Console.WriteLine("\nThank you, I found it! :)");
+
+            int wordcount = getWordcount_FromUser();
+
+            Console.WriteLine("This could take a while, please wait...");
+            Console.WriteLine(MemorablePasswordGenerator.GenerateWords(listname, wordcount));
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
+
+
+
+        private static int getWordcount_FromUser() {
+            Console.Write("\nSo what's the number of words you want to generate?" +
+                "\n>");
+            int wordcount = 1;
+            try
+            {
+                wordcount = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("That hasn't been a number ;), pls try again");
+                return getWordcount_FromUser();
+            }
+
+            return wordcount;
+        }
+
+
     }
 }
